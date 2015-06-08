7a9df9bc747e42231236d7203a7bac00fedde035
Junru Feng <541639970@qq.com>
2015-06-08 22:57:06 +0800

    Create Interface.cs

---
+++ Interface.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public static class Interface 
+{
+     
+	public static void Test1()
+	{
+		Debug.Log ("test111");
+	}
+	public static void Test2()
+	{
+		Debug.Log ("test2");
+	}
+}
